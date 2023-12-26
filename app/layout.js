0ca7bedd4c0f64163621c364a0059f8dbd109c44
Titipat <work@titipat.net>
2023-12-26 14:26:03 +0700

    update layout.js and pnpm-lock.yaml

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { useEffect } from "react";
 
 const font = Open_Sans({ subsets: ["latin"], display: "block" });
 
-export const metadata = {
-  title: "Titipat.net",
-  description: "I'm Titipat, full-stack web developer from Thailand",
-};
-
 export default function RootLayout({ children }) {
+  const pathname = usePathname();
+  useEffect(() => {
+    window.location = `https://titipat.net${pathname}`;
+  });
   return (
     <html lang="en">
       <body className={`${font.className}`}>
